22d5b9eb834a71e949e98519a1b9522cd4784bd5
SungHoon Jung <32007781+wooing1084@users.noreply.github.com>
2023-03-27 13:17:08 +0900

    back origin

--- testServer/testServer.py
+++ testServer/testServer.py
@@ -6,21 +6,7 @@ import os
 class FileUploadHandler(tornado.web.RequestHandler):
     def post(self):
         file_dict = self.request.files['file'][0]
-        deviceID = self.get_argument("deviceID")
-        battery = self.get_argument("battery")
-        received_ts = self.get_argument("timestamp")
-        filter_beginning = str(file_dict['body']).replace("b'", "")
-        filter_mid = filter_beginning.replace("\\r\\", "\n")
-        filter_last = filter_mid.replace("'", "")
-
         file_name = file_dict['filename']
-
-        print('Data received at:', received_ts, ' from:', deviceID)
-        print('File received:', file_name)
-        print('battery_percent:', battery)
-        print('--------------------------')
-        print(deviceID)
-
         file_body = file_dict['body']
         file_path = os.path.join(os.path.dirname(__file__), file_name)
         with open(file_path, 'wb') as f:
