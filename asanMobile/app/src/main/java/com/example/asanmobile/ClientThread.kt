b21bd0d5a5cb169919115115d6cc0b9872031cc4
SungHoon Jung <32007781+wooing1084@users.noreply.github.com>
2023-04-09 02:36:56 +0900

    아산병원 서버 post양식 맞춤

--- asanMobile/app/src/main/java/com/example/asanmobile/ClientThread.kt
+++ asanMobile/app/src/main/java/com/example/asanmobile/ClientThread.kt
@@ -8,24 +8,36 @@ import kotlin.concurrent.thread
 
 
 class ClientThread{
+    //val urlText = "http://172.16.226.109:8000/csv"
     val urlText = "http://10.0.2.2:8000/csv"
     lateinit var netConn: HttpURLConnection
 
 
-    public fun sendCSV(){
-        val data = arrayOf(arrayOf("Name", "Age", "Email"), arrayOf("John", "30", "john@example.com"), arrayOf("Jane", "25", "jane@example.com"))
 
+    public fun makeCSV(name: String, data: String): File {
         // 파일 생성 및 쓰기
         val dir = "data/data/com.example.asanmobile/"
-        var file = File(dir,"data.csv")
+        val file = File(dir,name)
         FileWriter(file)
         val writer = CSVWriter(FileWriter(file))
 
-        writer.writeAll(data.toMutableList())
+        writer.writeNext(arrayOf(data))
 
         writer.close()
+        return file
+    }
 
-        uploadFile(file, urlText)
+    public fun sendCSV(){
+        val data = "This is test data!!"
+        val file1 = makeCSV("ppg_lastSaveTime.csv", data)
+        val file2 = makeCSV("hrm_lastSaveTime.csv", data)
+        val file3 = makeCSV("acc_lastSaveTime.csv", data)
+
+
+        uploadFile(file1, urlText)
+        uploadFile(file2, urlText)
+        uploadFile(file3, urlText)
+
     }
 
     // 서버에 Post로 파일 전송하는 부분
@@ -34,6 +46,9 @@ class ClientThread{
         val requestBody = MultipartBody.Builder()
             .setType(MultipartBody.FORM)
             .addFormDataPart("file", file.name, RequestBody.create(MediaType.parse("application/octet-stream"), file))
+            .addFormDataPart("deviceID", "1234")
+            .addFormDataPart("battery", "100")
+            .addFormDataPart("timestamp", "2023-03-27")
             .build()
         //Post요청 생성
         val request = Request.Builder()
