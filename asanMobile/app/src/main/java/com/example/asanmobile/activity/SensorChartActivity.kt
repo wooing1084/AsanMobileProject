ae5193601a9d104fe7c3647b6f108272b735fa45
SungHoon Jung <32007781+wooing1084@users.noreply.github.com>
2023-06-29 02:03:14 +0900

    버퍼 차트

--- asanMobile/app/src/main/java/com/example/asanmobile/activity/SensorChartActivity.kt
+++ asanMobile/app/src/main/java/com/example/asanmobile/activity/SensorChartActivity.kt
@@ -1,6 +1,10 @@
 package com.example.asanmobile.activity
 
+import android.content.BroadcastReceiver
 import android.content.ContentValues.TAG
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.os.FileObserver
 import android.util.Log
@@ -13,6 +17,9 @@ import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
 import com.opencsv.CSVReader
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.io.InputStreamReader
 
 
@@ -24,6 +31,11 @@ class SensorChartActivity : AppCompatActivity() {
 
     private lateinit var ppgGreenChart: LineChart
     private lateinit var heartRateChart: LineChart
+    private lateinit var ppgGreenBufferChart : LineChart
+    private lateinit var heartRateBufferChart : LineChart
+
+    val ppgGreenBufferQueue = ArrayDeque<Entry>()
+    val heartBufferQueue = ArrayDeque<Entry>()
 
     // 데이터 담을 리스트
 //    private var heartIndex: Float = 0F
@@ -36,6 +48,9 @@ class SensorChartActivity : AppCompatActivity() {
 
         ppgGreenChart = binding.chartPpgGreen
         heartRateChart = binding.chartHeart
+
+        ppgGreenBufferChart = binding.chartNowPpgGreen
+        heartRateBufferChart = binding.chartNowHeart
 
        makeStatisticsChart()
 
@@ -50,8 +65,72 @@ class SensorChartActivity : AppCompatActivity() {
         }
         fileObserver.startWatching()
 
+        makeBufferChart()
 
-//        registerReceiver(receiver, IntentFilter())
+
+
+
+
+    }
+
+    private fun makeBufferChart(){
+        ppgGreenBufferChart.clear()
+        heartRateBufferChart.clear()
+
+        val ppgFileName = CsvController.getExistFileName(this, "HeartRate")
+        val heartFileName = CsvController.getExistFileName(this, "PpgGreen")
+
+        if(ppgFileName == null || heartFileName == null) {
+            Log.d(TAG, "No buffered file.")
+            return
+        }
+
+        val ppgFile = CsvController.getFile(CsvController.getExternalPath(this,"sensor")+ "/" + ppgFileName)
+        val heartFile = CsvController.getFile(CsvController.getExternalPath(this,"sensor") + "/" + heartFileName)
+
+        val ppgReader = CSVReader(InputStreamReader(ppgFile?.inputStream()))
+        val heartReader = CSVReader(InputStreamReader(heartFile?.inputStream()))
+
+        val ppgList = ppgReader.readAll()
+        ppgList.removeAt(0)
+        val heartList = heartReader.readAll()
+        heartList.removeAt(0)
+
+        val ppgGreenQueue = ArrayDeque<Entry>()
+        val heartQueue = ArrayDeque<Entry>()
+
+        var i = 0
+        for (ppg in ppgList) {
+            i++
+            val time = ppg[0].toFloat()
+            val data = ppg[1].toFloat()
+            ppgGreenQueue.add(Entry(i.toFloat(), data))
+//            ppgGreenQueue.add(Entry(time, data))
+
+        }
+
+        i = 0
+        for (heart in heartList) {
+            i++
+            val time = heart[0].toFloat()
+            val data = heart[1].toFloat()
+            heartQueue.add(Entry(i.toFloat(), data))
+//            ppgGreenQueue.add(Entry(time, data))
+        }
+
+        val ppgGreenDataSet = LineDataSet(ppgGreenQueue.toList(), "ppgGreen")
+        val heartDataSet = LineDataSet(heartQueue.toList(), "heartRate")
+
+        val ppgGreenData = LineData(ppgGreenDataSet)
+        val heartData = LineData(heartDataSet)
+
+        ppgGreenBufferChart.data = ppgGreenData
+        heartRateBufferChart.data = heartData
+
+        ppgGreenBufferChart.invalidate()
+        heartRateBufferChart.invalidate()
+
+        registerReceiver(receiver, IntentFilter())
     }
 
     private fun makeStatisticsChart(){
@@ -108,72 +187,72 @@ class SensorChartActivity : AppCompatActivity() {
     }
 
     //들어오는 즉시 그래프를 그리기 위한 현석이형 코드
-//    // service와 통신하기 위한 BroadcastReceiver
-//    private val receiver = object : BroadcastReceiver() {
-//        override fun onReceive(context: Context?, intent: Intent?) {
-//            if (Intent.ACTION_ATTACH_DATA.equals(intent?.action)) {
-//                val receivedString = intent?.getStringExtra("data")
-//                Log.d(TAG, "received: " + receivedString.toString())
-//
-//                val hrList = regexManager.hrRegex.findAll(receivedString.toString())
-//                val pgList = regexManager.pgRegex.findAll(receivedString.toString())
-//
-//                CoroutineScope(Dispatchers.IO).launch {
-//                    try {
-//                        for (hrPattern in hrList) {
-//                            val hrVal = hrPattern.value
-//                            val resRex = regexManager.valueRegex.find(hrVal)
-//                            val res = resRex?.value
-//
-//                            val dataMap = regexManager.dataExtract(res!!)
-//                            heartQueue.add(Entry(dataMap.time.toFloat(), dataMap.data))
-//
-//                            val heartSet: LineDataSet =
-//                                LineDataSet(heartQueue, "Heart") // 데이터 넣기 // LineDataSet 변환
-//
-//                            val heartData = LineData() // 차트에 담길 데이터
-//                            heartData.addDataSet(heartSet)
-//
-//                            // 차트에 데이터 추가
-//                            heartRateChart.data = heartData
-//                            heartRateChart.setTouchEnabled(false) // 차트 터치 disable
-//                            heartRateChart.invalidate() // 차트 업데이트
-//                        }
-//                    } catch (e: Exception) {
-//                        e.printStackTrace()
-//                    }
-//                }
-//
-//                CoroutineScope(Dispatchers.IO).launch {
-//                    try {
-//                        for (pgPattern in pgList) {
-//                            val pgVal = pgPattern.value
-//                            val resRex = regexManager.valueRegex.find(pgVal)
-//                            val res = resRex?.value
-//
-//                            val dataMap = regexManager.dataExtract(res!!)
-//                            ppgGreenQueue.add(Entry(dataMap.time.toFloat(), dataMap.data))
-//
-//                            val ppgGreenSet = LineDataSet(
-//                                    ppgGreenQueue,
-//                                    "ppgGreenArr"
-//                                ) // 데이터 넣기 // LineDataSet 변환
-//
-//                            val ppgGreenData = LineData() // 차트에 담길 데이터
-//                            ppgGreenData.addDataSet(ppgGreenSet)
-//
-//                            // 차트에 데이터 추가
-//                            ppgGreenChart.data = ppgGreenData
-//                            ppgGreenChart.setTouchEnabled(false) // 차트 터치 disable
-//                            ppgGreenChart.invalidate() // 차트 업데이트
-//
-//                        }
-//                    } catch (e: Exception) {
-//                        e.printStackTrace()
-//                    }
-//                }
-//            }
-//        }
-//    }
+    // service와 통신하기 위한 BroadcastReceiver
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (Intent.ACTION_ATTACH_DATA.equals(intent?.action)) {
+                val receivedString = intent?.getStringExtra("data")
+                Log.d(TAG, "received: " + receivedString.toString())
+
+                val hrList = regexManager.hrRegex.findAll(receivedString.toString())
+                val pgList = regexManager.pgRegex.findAll(receivedString.toString())
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        for (hrPattern in hrList) {
+                            val hrVal = hrPattern.value
+                            val resRex = regexManager.valueRegex.find(hrVal)
+                            val res = resRex?.value
+
+                            val dataMap = regexManager.dataExtract(res!!)
+                            heartBufferQueue.add(Entry(dataMap.time.toFloat(), dataMap.data))
+
+                            val heartSet: LineDataSet =
+                                LineDataSet(heartBufferQueue, "Heart") // 데이터 넣기 // LineDataSet 변환
+
+                            val heartData = LineData() // 차트에 담길 데이터
+                            heartData.addDataSet(heartSet)
+
+                            // 차트에 데이터 추가
+                            heartRateChart.data = heartData
+                            heartRateChart.setTouchEnabled(false) // 차트 터치 disable
+                            heartRateChart.invalidate() // 차트 업데이트
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+
+                CoroutineScope(Dispatchers.IO).launch {
+                    try {
+                        for (pgPattern in pgList) {
+                            val pgVal = pgPattern.value
+                            val resRex = regexManager.valueRegex.find(pgVal)
+                            val res = resRex?.value
+
+                            val dataMap = regexManager.dataExtract(res!!)
+                            ppgGreenBufferQueue.add(Entry(dataMap.time.toFloat(), dataMap.data))
+
+                            val ppgGreenSet = LineDataSet(
+                                ppgGreenBufferQueue,
+                                    "ppgGreenArr"
+                                ) // 데이터 넣기 // LineDataSet 변환
+
+                            val ppgGreenData = LineData() // 차트에 담길 데이터
+                            ppgGreenData.addDataSet(ppgGreenSet)
+
+                            // 차트에 데이터 추가
+                            ppgGreenChart.data = ppgGreenData
+                            ppgGreenChart.setTouchEnabled(false) // 차트 터치 disable
+                            ppgGreenChart.invalidate() // 차트 업데이트
+
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }
+            }
+        }
+    }
 }
 
